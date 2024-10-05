868ec155531514f5bff473fb52f7053e64a3b449
Mohib <mohibullahmohim2020@gmail.com>
2024-10-06 01:22:27 +0600

    service card added

---
+++ swift-pay-client/src/Components/Loader/Loader.jsx
@@ -0,0 +1,10 @@
+import {ScaleLoader} from "react-spinners"
+const Loader = () => {
+    return (
+        <div className="flex justify-center items-center h-screen">
+            <ScaleLoader />
+        </div>
+    );
+};
+
+export default Loader;
--- swift-pay-client/src/Components/ServiceCardSlider/ServiceCardSlider.jsx
+++ swift-pay-client/src/Components/ServiceCardSlider/ServiceCardSlider.jsx
@@ -1,116 +1,87 @@
+import axios from "axios";
+import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-
-import Slider from "react-slick";
 import { useQuery } from "@tanstack/react-query";
 
+import Loader from "../Loader/Loader";
 
 const ServiceCardSlider = () => {
-    const settings = {
-        infinite: true,
-        slidesToShow: 3, // default for mobile
-        slidesToScroll: 1,
-        autoplay: true,
-        speed: 2500,
-        autoplaySpeed: 5000,
-        cssEase: "linear",
-        responsive: [
-          {
-            breakpoint: 1024, // for screens larger than 1024px (desktop)
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 1,
-            },
-          },
-          {
-            breakpoint: 600, // for screens between 600px and 1024px (tablet)
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 1,
-            },
-          },
-          {
-            breakpoint: 480, // for screens smaller than 600px (mobile)
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-            },
-          },
-        ],
-      };
-const {data: servicesData, isLoading} = useQuery()
+  const settings = {
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 2500,
+    autoplaySpeed: 5000,
+    cssEase: "linear",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
+  const { data: servicesData, isLoading } = useQuery({
+    queryKey: ["service"],
+    queryFn: async () => {
+      const response = await axios.get("/serviceData.json");
+      return response?.data;
+    },
+  });
+
+  if (isLoading) return <Loader />;
 
   return (
-    <div className="slider-container w-full mx-auto p-4 md:p-8 lg:p-12">
+    <div className="slider-container w-full mx-auto p-4 md:p-4 lg:p-8">
+      <div className="text-center my-10">
+        <h1 className="text-2xl md:text-4xl font-semibold text-tarnary">Our Services</h1>
+        <p className="text-base md:text-lg text-tarnary" >3 easy steps to use your swiftPay Account</p>
+      </div>
       <Slider {...settings}>
-
-        {/* slider one */}            
-          <div className="bg-white w-full mx-4 md:mx-auto mt-16">
-            <div className="px-8 py-6 mx-3 md:p-10 border-2 border-tarnary">
-              <h1 className="text-4xl md:text-5xl font-medium leading-tight text-tarnary mb-6">
-              we commit to a dynamic work environment to support
-              </h1>
-              <p className="text-base md:text-lg font-normal leading-normal text-tarnary mb-6">
-              What is Cash in
-              </p>
-              <div className="flex justify-center">
-                <span className="bg-secondary rounded-lg text-tarnary font-medium text-base md:text-lg py-3 px-8 md:px-12 hover:bg-primary transition-all duration-150 ease-in-out cursor-pointer " >Cash In</span>
+        {servicesData?.map((service) => (
+          <div key={service.id}>
+            {/* Each slide content */}
+            <div className="bg-white md:mx-2 md:mt-5">
+              <div className="px-4 py-6 mx-2 border-2 border-tarnary rounded-md">
+                <h1 className="text-2xl md:text-4xl font-semibold text-tarnary mb-6">
+               {service.service}
+                </h1>
+                <p className="text-base md:text-lg font-normal text-tarnary mb-6">
+                  {service.Title}
+                </p>
+                <div className="flex justify-center">
+                  <span className="bg-secondary rounded-lg text-tarnary font-medium text-base md:text-lg py-3 px-8 md:px-12 hover:bg-primary transition-all duration-150 ease-in-out cursor-pointer">
+                    {service.service_name}
+                  </span>
+                </div>
+                <p className="text-sm md:text-base font-normal text-tarnary mt-2">
+                  Cash In refers to the process of depositing an amount into a
+                  swiftPay account from swiftpay-authorized Uddokta Point.
+                  {service.description}
+                </p>
               </div>
-              <p className="text-base md:text-lg font-normal leading-normal text-tarnary mt-6">
-                Hi,
-              </p>
-              <p className="text-sm md:text-base font-normal leading-normal text-tarnary mt-2">
-              Cash In refers to the process of depositing an amount into a swiftPay account from swiftpay-authorized Uddokta Point.
-              </p>
             </div>
           </div>
-
-        {/* slider two */}
-        <div>
-          <div className="bg-white w-full mx-4 md:mx-auto mt-16">
-            <div className="px-8 py-6 mx-3 md:p-10 border-2 border-tarnary">
-              <h1 className="text-4xl md:text-5xl font-medium leading-tight text-tarnary mb-6">
-                Lets go, its Google I/O
-              </h1>
-              <p className="text-base md:text-lg font-normal leading-normal text-tarnary mb-6">
-              What is cash out
-              </p>
-              <div className="flex justify-center">
-              <span className="bg-secondary rounded-lg text-tarnary font-medium text-base md:text-lg py-3 px-8 md:px-12 hover:bg-primary transition-all duration-150 ease-in-out cursor-pointer " >Cash Out </span>
-              </div>
-              <p className="text-base md:text-lg font-normal leading-normal text-tarnary mt-6">
-                Hi,
-              </p>
-              <p className="text-sm md:text-base font-normal leading-normal text-tarnary mt-2">
-              Cash Out refers to the process of withdrawing cash from one’s own swifPay account by visiting a nearby swifPay Uddokta. Customers can initiate Cash Out.
-              </p>
-            </div>
-          </div>
-        </div>
-
-        {/* slider three */}
-        <div>
-          <div className="bg-white w-full mx-4 md:mx-auto mt-16">
-            <div className="px-8 py-6 mx-3 md:p-10 border-2 border-tarnary">
-              <h1 className="text-4xl md:text-5xl font-medium leading-tight text-tarnary mb-6">
-                Lets go, its Google I/O
-              </h1>
-              <p className="text-base md:text-lg font-normal leading-normal text-tarnary mb-6">
-                What is Send Money
-              </p>
-              <div className="flex justify-center">
-              <span className="bg-secondary rounded-lg text-tarnary font-medium text-base md:text-lg py-3 px-8 md:px-12 hover:bg-primary transition-all duration-150 ease-in-out cursor-pointer" >Send Money</span>
-              </div>
-              <p className="text-base md:text-lg font-normal leading-normal text-tarnary mt-6">
-                Hi,
-              </p>
-              <p className="text-sm md:text-base font-normal leading-normal text-tarnary mt-2">
-              Send Money is a feature on Nagad that enables one to send money to any mobile number.
-              </p>
-            </div>
-          </div>
-        </div>
-
+        ))}
       </Slider>
     </div>
   );
