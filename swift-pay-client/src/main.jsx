3602be524d1b78db8f5fcdea17074b598dc8f7e6
developerMohib <mohibullahmohim2020@gmail.com>
2026-07-16 18:21:47 +0600

    Update router in main jsx

--- swift-pay-client/src/main.jsx
+++ swift-pay-client/src/main.jsx
@@ -7,12 +7,14 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const queryClient = new QueryClient();
 
 import "./index.css";
+import AuthProvider from "./authProvider/AuthProvider";
+import { appRouter } from "./Routes/Routes";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
       <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
+        <RouterProvider router={appRouter} />
       </QueryClientProvider>      
     </AuthProvider>
   </React.StrictMode>
